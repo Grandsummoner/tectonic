11bfada198aa6a437f146aff373795db04a6a04c
Grandsummoner <gsnextninja@gmail.com>
2026-07-12 10:58:03 +0800

    Update PluginEditor.h

--- source/PluginEditor.h
+++ source/PluginEditor.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
-#include "TectonicChannel.h" // Include our channel class
+#include "TectonicChannel.h"
 
 class TectonicAudioProcessorEditor  : public juce::AudioProcessorEditor
 {
@@ -12,11 +12,11 @@ public:
     void paint (juce::Graphics&) override;
     void resized() override;
 
+    int getFocusedChannel() const;
+
 private:
     TectonicAudioProcessor& audioProcessor;
     std::unique_ptr<juce::Drawable> backgroundSvg;
-
-    // Our array of 8 modular channels
     std::array<std::unique_ptr<TectonicChannel>, 8> channels;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (TectonicAudioProcessorEditor)
