d2935241393d8f09ff675dcca0297d8d2be65fa2
Grandsummoner <gsnextninja@gmail.com>
2026-07-12 10:15:59 +0800

    Update PluginProcessor.h

--- source/PluginProcessor.h
+++ source/PluginProcessor.h
@@ -1,12 +1,45 @@
-class TectonicAudioProcessor : public juce::AudioProcessor
+#pragma once
+
+#include <JuceHeader.h>
+
+class TectonicAudioProcessor  : public juce::AudioProcessor
 {
 public:
     TectonicAudioProcessor();
-    // ... other standard processor functions ...
+    ~TectonicAudioProcessor() override;
+
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+
+    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+
+    juce::AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override;
+
+    const juce::String getName() const override;
+
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    bool isMidiEffect() const override;
+    double getTailLengthSeconds() const override;
+
+    int getNumPrograms() override;
+    int getCurrentProgram() override;
+    void setCurrentProgram (int index) override;
+    const juce::String getProgramName (int index) override;
+    void changeProgramName (int index, const juce::String& newName) override;
+
+    void getStateInformation (juce::MemoryBlock& destData) override;
+    void setStateInformation (const void* data, int sizeInBytes) override;
+
+    // Static layout helper function to generate our parameter map
+    static juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
+
+    // ValueTree containing all parameter data
+    juce::AudioProcessorValueTreeState apvts;
 
 private:
-    juce::AudioFormatManager formatManager;
-    
-    // An AudioSampleBuffer to hold the loaded sound in RAM
-    juce::AudioSampleBuffer kickSampleBuffer; 
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (TectonicAudioProcessor)
 };
